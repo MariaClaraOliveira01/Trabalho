d92dd4a9a96eb53ca32c9aa7d842ece402afc9de
MariaClaraOliveira01 <165787108+MariaClaraOliveira01@users.noreply.github.com>
2024-11-06 08:39:48 -0300

    oi

--- script.js
+++ script.js
@@ -6,23 +6,23 @@ document.getElementById("text").innerText = "Ao clicar, eu mudei!!";
 // Adiciona um novo parágrafo ao final do corpo
 function adicionarParagrafo() {
     const novoParagrafo = document.createElement("p");
-    novoParagrafo.innerText = "Depois que voçê clicou, eu apareci 😊 !!";
+    novoParagrafo.innerText = "Depois que você clicou, eu apareci 😊 !!";
     document.body.appendChild(novoParagrafo);
 }
 
 // Remove o último parágrafo, se houver mais de um
 function removerParagrafo() {
     const paragrafos = document.querySelectorA11("p");
-    if (paragrafos.lenght > 1) {
-        paragrafos[paragrafos.lenght - 1].remove();
+    if (paragrafos.length > 1) {
+        paragrafos[paragrafos.length - 1].remove();
     } else {
         alert("Não há mais parágrafo para remover 😢 !!");
     }
 }
 
-//Muda a cor de funfo da página para uma cor aleatória
+//Muda a cor de fundo da página para uma cor aleatória
 function mudarCorFundo (){
-    const corAleatoria =  '#${Math.floor(Math.random() = 16777215).toString(16)}';
+    const corAleatoria =   `#${Math.floor(Math.random() = 16777215).toString(16)}`;
     document.body.style.backgroundColor = corAleatoria;
 }
 
